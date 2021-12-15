77882dbff94bb006f1b39b67aa594995f44822c1
Laura Timmermans <timmermans_laura@hotmail.com>
2021-12-15 17:22:47 +0100

    Photo lint added

--- photo.js
+++ photo.js
@@ -1,72 +1,129 @@
-/*
-const enlarged = document.querySelectorAll(".enlarged");
-const prevBtn = document.querySelector(".prevBtn");
-const nextBtn = document.querySelector(".nextBtn");
+/* LARGE IMAGE */
 
-
-enlarged.forEach(function(enlarged, index){
-    enlarged.style.left = `${index*100}%`;
-});
-
-let counter = 0;
-nextBtn.addEventListener("click", function(){
-    counter++;
-    carousel();
-});
-prevBtn.addEventListener("click", function(){
-    counter--;
-    carousel();
-});
-
-function carousel(){
-
-    if (counter === enlarged.length){
-        counter = 0;
-    }
-    if (counter < 0) {
-        counter = enlarged.length-1;
-    }
-    enlarged.forEach(function(enlarged){
-        enlarged.style.transform = `translateX(-${counter*100}%)`;
-    });
-};
-*/
-
-const photos = ["astronomy", "chipmunk", "fox", "landscape", "mountain", "national-park", "river"];
-
-var slideIndex = 1;
-    displaySlide(slideIndex);
+var imageIndex = 1;
+    displayImage(imageIndex);
    
-    function moveSlides(n) {
-        displaySlide(slideIndex += n);
+    function moveImages(n) {
+        displayImage(imageIndex += n);
     }
    
-    function activeSlide(n) {
-        displaySlide(slideIndex = n);
+    function activeImage(n) {
+        displayImage(imageIndex = n);
     }
    
     /* Main function */
-    function displaySlide(n) {
+    function displayImage(n) {
         var i;
-        var totalslides = 
-            document.getElementsByClassName("slide");
-        var totaldots = 
+        var totalImages = 
+            document.getElementsByClassName("image");
+        var totalDots = 
             document.getElementsByClassName("footerdot");
           
-        if (n > totalslides.length) {
-            slideIndex = 1;
+        if (n > totalImages.length) {
+            imageIndex = 1;
         }
           
         if (n < 1) {
-            slideIndex = totalslides.length;
+            imageIndex = totalImages.length;
         }
-        for (i = 0; i < totalslides.length; i++) {
-            totalslides[i].style.display = "none";
+        for (i = 0; i < totalImages.length; i++) {
+            totalImages[i].style.display = "none";
         }
-        for (i = 0; i < totaldots.length; i++) {
-            totaldots[i].className = 
-            totaldots[i].className.replace(" active", "");
+        for (i = 0; i < totalDots.length; i++) {
+            totalDots[i].className = 
+            totalDots[i].className.replace(" active", "");
         }
-        totalslides[slideIndex - 1].style.display = "block";
-        totaldots[slideIndex - 1].className += " active";
+        totalImages[imageIndex - 1].style.display = "block";
+        totalDots[imageIndex - 1].className += " active";
     }
+
+    var faStroke = document.getElementsByClassName("fa-stroke");
+      addEventListener("mouseover", function () {
+        displayStroke(faStroke);
+      } );
+
+
+
+    /* CARD LINT */
+
+    (function ($) {
+        $(function () {
+          var slider = $(".slider").flickity({
+            imagesLoaded: true,
+            percentPosition: false,
+            prevNextButtons: false, //true = enable buttons
+            initialIndex: 3,
+            pageDots: false, //true = enable dots
+            groupCells: 1,
+            selectedAttraction: 0.2,
+            friction: 0.8,
+            draggable: true
+          });
+      
+          //this enables clicking on cards
+          slider.on(
+            "staticClick.flickity",
+            function (event, pointer, cellElement, cellIndex) {
+              if (typeof cellIndex == "number") {
+                slider.flickity("selectCell", cellIndex);
+              }
+            }
+          );
+      
+          //this resizes the cards and centers the lint-container because it tends to move a few pixels to the right if .resize() and .reposition() aren't used
+           var flkty = slider.data("flickity");
+          /* flkty.selectedElement.classList.add("is-custom-selected"); */
+          /* flkty.resize(); */
+          flkty.reposition();
+          let time = 0;
+          function reposition() {
+            flkty.reposition();
+            if (time++ < 10) {
+              requestAnimationFrame(reposition);
+            } else {
+              $(".flickity-prev-next-button").css("pointer-events", "auto");
+            }
+          }
+          requestAnimationFrame(reposition);
+      
+          //this expands the cards when in focus
+          /* flkty.on("settle", () => {
+            $(".card").removeClass("is-custom-selected");
+            $(".flickity-prev-next-button").css("pointer-events", "none");
+            flkty.selectedElement.classList.add("is-custom-selected");
+      
+            let time = 0;
+            function reposition() {
+              flkty.reposition();
+              if (time++ < 10) {
+                requestAnimationFrame(reposition);
+              } else {
+                $(".flickity-prev-next-button").css("pointer-events", "auto");
+              }
+            }
+            requestAnimationFrame(reposition);
+          });
+          */
+      
+          //this reveals the lint-container
+          $(".lint-container").addClass("animation-reveal");
+          $(".lint-container").css("opacity", "0");
+          flkty.resize();
+          flkty.reposition();
+          setTimeout(() => {
+            $(".lint-container").removeClass("animation-reveal");
+            $(".lint-container").css("opacity", "1");
+            flkty.resize();
+            flkty.reposition();
+            let time = 0;
+            function reposition() {
+              flkty.reposition();
+              if (time++ < 10) {
+                requestAnimationFrame(reposition);
+              }
+            }
+            requestAnimationFrame(reposition);
+          }, 1000);
+        });
+      })(jQuery);
+      
