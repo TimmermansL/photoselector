72b01298d5ae8d59b5c456c1e448c9954c8a48de
Laura Timmermans <timmermans_laura@hotmail.com>
2022-01-17 17:46:23 +0100

    Slider works

--- photo.js
+++ photo.js
@@ -1,6 +1,7 @@
 "use strict";
 
-/* PHOTO ARRAY */
+/* SELECTING ELEMENTS */
+/* Loop through photo array */
 const photos = [
   "photo-0.jpg",
   "photo-1.jpg",
@@ -9,12 +10,14 @@ const photos = [
   "photo-4.jpg",
   "photo-5.jpg",
   "photo-6.jpg",
+  "photo-7.jpg",
+  "photo-8.jpg",
+  "photo-9.jpg",
 ];
-
 for (let i = 0; i < photos.length; i++) {
   document.getElementById(`photo${[i]}`).src = "./images/" + photos[i];
 }
-/* SELECTING ELEMENTS */
+
 const imageLarge = document.getElementById("image-large");
 const imageSmall = document.querySelectorAll(".image-small");
 
@@ -26,7 +29,7 @@ const init = function () {
 };
 init();
 
-/* PREVIOUS / NEXT BUTTONS */
+/* PREVIOUS / NEXT BUTTONS large image */
 const moveImage = function (side) {
   if (side === 1) {
     imageIndex++;
@@ -39,7 +42,7 @@ const moveImage = function (side) {
   }
 };
 
-/* CARD LINT */
+/* IMAGE LINT connect with large image */
 const activeImage = function (imageIndex) {
   imageLarge.src = `./images/photo-${imageIndex}.jpg`;
   for (let j = 0; j < imageSmall.length; j++)
@@ -79,86 +82,3 @@ slider.addEventListener("mousemove", (e) => {
   const walk = (x - startX) * scrollSpeed;
   slider.scrollLeft = scrollLeft - walk;
 });
-
-//////////////////////////////////////
-// (function ($) {
-//   $(function () {
-//     var slider = $(".slider").flickity({
-//       imagesLoaded: true,
-//       percentPosition: false,
-//       prevNextButtons: false, //true = enable buttons
-//       initialIndex: 3,
-//       pageDots: false, //true = enable dots
-//       groupCells: 1,
-//       selectedAttraction: 0.2,
-//       friction: 0.8,
-//       draggable: true,
-//     });
-
-//     //this enables clicking on cards
-//     slider.on(
-//       "staticClick.flickity",
-//       function (event, pointer, cellElement, cellIndex) {
-//         if (typeof cellIndex == "number") {
-//           slider.flickity("selectCell", cellIndex);
-//         }
-//       }
-//     );
-
-//     //this resizes the cards and centers the lint-container because it tends to move a few pixels to the right if .resize() and .reposition() aren't used
-//     var flkty = slider.data("flickity");
-//     /* flkty.selectedElement.classList.add("is-custom-selected"); */
-//     /* flkty.resize(); */
-//     flkty.reposition();
-//     let time = 0;
-//     function reposition() {
-//       flkty.reposition();
-//       if (time++ < 10) {
-//         requestAnimationFrame(reposition);
-//       } else {
-//         $(".flickity-prev-next-button").css("pointer-events", "auto");
-//       }
-//     }
-//     requestAnimationFrame(reposition);
-
-//     // this expands the cards when in focus
-//     /* flkty.on("settle", () => {
-//             $(".card").removeClass("is-custom-selected");
-//             $(".flickity-prev-next-button").css("pointer-events", "none");
-//             flkty.selectedElement.classList.add("is-custom-selected");
-
-//             let time = 0;
-//             function reposition() {
-//               flkty.reposition();
-//               if (time++ < 10) {
-//                 requestAnimationFrame(reposition);
-//               } else {
-//                 $(".flickity-prev-next-button").css("pointer-events", "auto");
-//               }
-//             }
-//             requestAnimationFrame(reposition);
-//           });
-//           */
-
-//     // NO this reveals the lint-container
-//     $(".lint-container").addClass("animation-reveal");
-//     $(".lint-container").css("opacity", "0");
-//     flkty.resize();
-//     flkty.reposition();
-//     setTimeout(() => {
-//       $(".lint-container").removeClass("animation-reveal");
-//       $(".lint-container").css("opacity", "1");
-//       flkty.resize();
-//       flkty.reposition();
-//       let time = 0;
-//       function reposition() {
-//         flkty.reposition();
-//         if (time++ < 10) {
-//           requestAnimationFrame(reposition);
-//         }
-//       }
-//       requestAnimationFrame(reposition);
-//     }, 1000);
-//   });
-// })(jQuery);
-//
